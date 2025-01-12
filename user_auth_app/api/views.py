03221ae8b72ff4df514fb1b187e0bd866ef7293a
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-12 12:57:23 +0100

    added login view and url

--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -5,6 +5,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.authtoken.views import ObtainAuthToken
 
 
 class UserProfileList(generics.ListCreateAPIView):
@@ -26,11 +27,32 @@ class RegistrationView(APIView):
 
         if serializer.is_valid():
             saved_account = serializer.save()
-            token = Token.objects.get_or_create(user=saved_account)
+            token, created = Token.objects.get_or_create(user=saved_account)
             data= {
                 'token': token.key,
                 'username': saved_account.username,
                 'email': saved_account.email
+            }
+        else:
+            data=serializer.errors
+        
+        return Response(data)
+    
+
+class CustomLoginView(ObtainAuthToken):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            data= {
+                'token': token.key,
+                'username': user.username,
+                'email': user.email
             }
         else:
             data=serializer.errors
