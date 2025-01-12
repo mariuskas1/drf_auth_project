27530a8bb2b48c339685236f69457afb762e15a6
Marius Kasparek <marius.kasparek@gmail.com>
2025-01-12 12:45:35 +0100

    added registration view and url

--- user_auth_app/api/urls.py
+++ user_auth_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import UserProfileList, UserProfileDetail
+from .views import UserProfileList, UserProfileDetail, RegistrationView
 
 urlpatterns = [
     path('profiles/', UserProfileList.as_view(), name='userprofile-list'),
     path('profiles/<int:pk>/', UserProfileDetail.as_view(), name='userprofile-detail'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
 ]
--- user_auth_app/api/views.py
+++ user_auth_app/api/views.py
@@ -1,12 +1,38 @@
 from rest_framework import generics
 from user_auth_app.models import UserProfile
-from .serializers import UserProfileSerializer
+from .serializers import UserProfileSerializer, RegistrationSerializer
+from rest_framework.views import APIView
+from rest_framework.permissions import AllowAny
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
 class UserProfileList(generics.ListCreateAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
 
+
 class UserProfileDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserProfile.objects.all()
     serializer_class = UserProfileSerializer
+
+
+class RegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            saved_account = serializer.save()
+            token = Token.objects.get_or_create(user=saved_account)
+            data= {
+                'token': token.key,
+                'username': saved_account.username,
+                'email': saved_account.email
+            }
+        else:
+            data=serializer.errors
+        
+        return Response(data)
